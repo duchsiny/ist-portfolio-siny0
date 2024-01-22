a99a505cd2113f745f72121e2d39be2561c00361
dechacsiny duch <2027duch.dechacsin@fcboe.org>
2024-01-22 14:47:58 -0500

    message

---
+++ c/bsics.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+  
+int main()
+	
+{
+
+      
+
+
+
+        printf ("This is my C species filen\n");
+	int num;
+	printf("Ender a number: ");
+	scanf("%i", &num);
+        printf(" Your number was %i\n", num);
+			
+	return 0;
+
